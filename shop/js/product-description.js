cd8ad703a7898f15667b38c4bcba073208cb2cbb
VirBux <denny_ovcar@hotmail.com>
2019-09-15 19:21:04 +0200

    Bugfix Produkttinformationen

--- shop/js/product-description.js
+++ shop/js/product-description.js
@@ -1,34 +1,36 @@
 $(function() {
-        /* Selectors */
-        var overlay = $('.overlay-products')
-        var productDescription = $('.product-description')
-        var productImg = $('.product-img')
-        var closeProductContainer = $('.product-description-close-btn')
+    /* Selectors */
+    var body = $('body')
+    var overlay = $('.overlay-products')
+    var productDescription = $('.product-description')
+    var productImg = $('.product-img')
+    var closeProductContainer = $('.product-description-close-btn')
 
-        /* Clickevents */
+    /* Clickevents */
 
-        /* Klick auf das Produktbild */
-        productImg.on( "click", function() {
-            if(jQuery(this).siblings(".product-description").is(":visible")){
-                jQuery(this).removeClass('img-selected')
-            }else{
-                jQuery(this).addClass('img-selected')
-            }
-            jQuery(this).siblings(".product-description").toggle("fast")
-            overlay.toggle()
-        });
+    /* Klick auf das Produktbild */
 
-        /* Klick auf das Schliessen Icon */
-        closeProductContainer.on( "click", function() {
-            productImg.removeClass("img-selected")
-            jQuery(this).parent().parent(".product-description").toggle("fast")
-            overlay.toggle("fast")
-        });
+    body.on( "click",'.product-img', function() {
+        if(jQuery(this).siblings(".product-description").is(":visible")){
+            jQuery(this).removeClass('img-selected')
+        }else{
+            jQuery(this).addClass('img-selected')
+        }
+        jQuery(this).siblings(".product-description").toggle("fast")
+        overlay.toggle()
+    });
 
-        /* Dunkle Überlagerung */
-        overlay.on("click", function(){
-            overlay.toggle("fast")
-            productImg.removeClass("img-selected")
-            $('.product-description:visible').toggle("fast")
+    /* Klick auf das Schliessen Icon */
+    body.on( "click", '.product-description-close-btn', function() {
+        productImg.removeClass("img-selected")
+        jQuery(this).parent().parent(".product-description").toggle("fast")
+        overlay.toggle("fast")
+    });
+
+    /* Dunkle Überlagerung */
+    body.on("click", '.overlay-products', function(){
+        overlay.toggle("fast")
+        productImg.removeClass("img-selected")
+        $('.product-description:visible').toggle("fast")
     });
 });
