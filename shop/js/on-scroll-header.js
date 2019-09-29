6c88ea935b6bde28db57b3160d3f54ca062cae45
VirBux <denny_ovcar@hotmail.com>
2019-09-29 12:39:13 +0200

    Update

--- shop/js/on-scroll-header.js
+++ shop/js/on-scroll-header.js
@@ -1,10 +1,14 @@
-var root = document.documentElement;
-var headerLogo = document.getElementById("logo");
-var headerHeight = document.getElementById("page-header").offsetWidth;
-var headerLogoWidth = headerLogo.offsetWidth;
-var headerTitle = document.getElementsByClassName("nav-title");
+var root = document.documentElement; // Root preset for manipulation css root
 
-console.log(headerHeight)
+var headerLogo = document.getElementById("logo");
+var headerTitle = document.getElementsByClassName("nav-title");
+var pageNav = document.getElementById('page-main-nav');
+var vendorHeaderBackground = document.getElementsByClassName('additional-head-background');
+
+// Get start settings
+var midHeaderHeight = getComputedStyle(root).getPropertyValue('--mid-header-margin-top');
+var headerHeight = document.getElementById("page-header").offsetHeight;
+var headerLogoWidth = headerLogo.offsetWidth;
 
 
 // When the user scrolls down 50px from the top of the document, resize the header's font size
@@ -13,21 +17,32 @@ window.onscroll = function() {scrollFunction()}
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
 
+    // Header height
     root.style.setProperty('--header-height', 85 + "px");
-
+    // Logo Width
     headerLogo.style.width = "192.5px";
 
+    // Total Vendor part
+    vendorHeaderBackground[0].style.backgroundColor = "var(--main-col)"
+    root.style.setProperty('--mid-header-margin-top', 0 + "px");
+
     for(var i = 0; i < headerTitle.length; i++){
-        headerTitle[i].style.display = "none"; 
+      pageNav.className = "push-main-nav";
     }
 
   } else {
-    root.style.setProperty('--header-height', 160 + "px");
 
+    // Header height
+    root.style.setProperty('--header-height', headerHeight + "px");
+    // Logo width
     headerLogo.style.width = headerLogoWidth + "px";
 
+    // Total Vendor part
+    vendorHeaderBackground[0].style.backgroundColor = "transparent"
+    root.style.setProperty('--mid-header-margin-top', midHeaderHeight);
+
     for(var i = 0; i < headerTitle.length; i++){
-        headerTitle[i].style.display = "block"; 
+      pageNav.classList.remove("push-main-nav")
     }
 
   }
